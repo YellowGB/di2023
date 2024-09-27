7fef8b82b49df4c76ea88ffaaa1c31abfe10aca0
YellowGB <yellowchickencorp@gmail.com>
2024-09-27 14:12:33 +0200

    [CATALOGUE-fix] Wrong article added to the catalogue and fatal error on Singleton

--- Catalogue/Catalogue.php
+++ Catalogue/Catalogue.php
@@ -1,25 +1,30 @@
 <?php
 
-class Catalogue{
-    private static self $instance;
+class Catalogue
+{
+    private static ?self $instance = null;
     private array $articles;
 
-    private function __construct(array $SomeArticles){
+    private function __construct(array $SomeArticles)
+    {
         $this->articles = $SomeArticles;
     }
 
-    public static function getInstance(array $someArticles = []): self{
-        if (is_null(self::$instance )){
+    public static function getInstance(array $someArticles = []): self
+    {
+        if (is_null(self::$instance)) {
             self::$instance = new Catalogue($someArticles);
         }
         return self::$instance;
     }
 
-    public function addArticle(string $nom): bool{
+    public function addArticle(string $nom): bool
+    {
         $this->articles[] = $nom;
         return true;
     }
-    public function getArticles(){
+    public function getArticles()
+    {
         return $this->articles;
     }
 }
